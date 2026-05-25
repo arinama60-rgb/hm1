8e9f4618e7c273cc2ee26283b5095fa8873e1f5b
Laptopchik <arinama60@gmail.com>
2026-05-25 11:58:37 +0200

    lesson3.py

--- lesson3.py
+++ lesson3.py
@@ -1,124 +1,246 @@
-class Item:
-    def __init__(self, name):
+class Animal:
+    def __init__(self, name, age):
         self.name = name
-
-class Character:
-    def __init__(self, name, level, HP):
-        self.name = name
-        self.level = level
-        self.HP = HP
-        self.inventory = []
-
+        self.age = age
+        self.health = 100
+        self.is_alive = True
 
     def info(self):
-        print('Імя:',self.name)
-        print('Рівень:', self.level)
-        print('Здоровя:', self.HP)
+        print("Ім'я:", self.name)
+        print("Вік:", self.age)
+        print("HP:", self.health)
+        print("Живий:", "Так" if self.is_alive else "Ні")
 
-    def rest(self):
-        self.HP += 10
-        print(self.name, 'Відпочив і відновив здоровя. Тепер HP:', self.HP)
+    def sound(self):
+        print("Тварина видає звук")
 
-    def add_item(self, item):
-        self.inventory.append(item)
-
-    def show_inventory(self):
-        print('Інвентар ben|den|pen:')
-        if len(self.inventory) == 0:
-            print('Порожній')
+    def take_damage(self, damage):
+        self.health -= damage
+        if self.health <= 0:
+            self.health = 0
+            self.is_alive = False
+            print(self.name, "помер")
         else:
-            for item in self.inventory:
-                print(item.name)
-
-class Warrior(Character):
-    def __init__(self, name, level, HP, strength):
-        super().__init__(name, level, HP)
-        self.strength = strength
-
-    def attack(self, enemy):
-        damage = self.strength + 10
-        enemy.HP -= damage
-        print(self.name, 'б,є (-30HP)')
-
-    def shield(self):
-        print(self.name, 'використовує щит')
-
-class Mage(Character):
-    def __init__(self, name, level, HP, mana):
-        super().__init__(name, level, HP)
-        self.mana = mana
-
-    def attack(self, enemy):
-        if self.mana >= 10:
-            self.mana -= 10
-            enemy.HP -=20
-            print(self.name, 'використовує магію (-20HP)')
-        else:
-            print(self.name, 'нема мани')
-
-    def teleport(self):
-        print(self.name, 'телепортується')
-
-class Archer(Character):
-    def __init__(self, name, level, HP, energy):
-        super().__init__(name, level, HP)
-        self.energy = energy
-
-    def attack(self, enemy):
-        if self.energy >= 5:
-            self.energy -= 5
-            enemy.HP -=15
-            print(self.name, 'стріляє (-20HP)')
-        else:
-            print(self.name, 'нема єнергії')
-
-    def dodge(self):
-        print(self.name, 'ухиляється')
+            print(self.name, "отримав", damage, "шкоди. HP:", self.health)
 
 
+class WildAnimal(Animal):
+    def __init__(self, name, age, power):
+        super().__init__(name, age)
+        self.power = power
 
-hero1 = Warrior('Ben', 5, 100, 20, )
-hero2 = Mage('Den', 7, 90, 30)
-hero3 = Archer('Pen', 4, 70, 20)
+    def hunt(self, prey):
+        if not self.is_alive:
+            print(self.name, "мертвий і не може полювати")
+            return
+        if not prey.is_alive:
+            print(prey.name, "вже мертвий, нема сенсу полювати")
+            return
+        print(self.name, "полює на", prey.name)
+        damage = self.power
+        prey.take_damage(damage)
 
-heroes = [hero1, hero2, hero3]
 
-for hero in heroes:
-    hero.info()
-    hero.show_inventory()
-    hero.rest()
-    print('----------')
+class Predator(WildAnimal):
+    def __init__(self, name, age, power, claw_damage):
+        super().__init__(name, age, power)
+        self.claw_damage = claw_damage
 
-item1 = Item('Зілля регенерацї')
-item2 = Item('Зілля сили')
+    def hunt(self, prey):
+        if not self.is_alive:
+            print(self.name, "мертвий")
+            return
+        if not prey.is_alive:
+            print(prey.name, "вже мертвий")
+            return
+        total_damage = self.power + self.claw_damage
+        print(self.name, "атакує кігтями", prey.name, "на", total_damage, "шкоди")
+        prey.take_damage(total_damage)
 
-print('Виберіть предмет:')
-print('1 - Зілля регенерацї')
-print('2 - Зілля сили')
-choice = input('Ваш вибір: ')
-print('-------------------')
+    def roar(self):
+        print(self.name, "грізно реве")
 
-if choice == '1':
-    hero1.add_item(item1)
-    hero2.add_item(item1)
-    hero3.add_item(item1)
-elif choice == '2':
-    hero1.add_item(item2)
-    hero2.add_item(item2)
-    hero3.add_item(item2)
-else:
-    print('Немає такого предмета')
 
-print('Бажаєта побачити інвентар ВСІХ персонажів??')
-print('1 - Відкрити')
-print('2 - Вихід')
-choice2 = input('Ваш вибір: ')
+class Herbivorous(WildAnimal):
+    def __init__(self, name, age, power, speed):
+        super().__init__(name, age, power)
+        self.speed = speed
 
-if choice2 == '1':
-    hero1.show_inventory()
-    hero2.show_inventory()
-    hero3.show_inventory()
-elif choice2 == '2':
-    print('Вихід')
-else:
-    print('Помилка')
+    def run_away(self):
+        print(self.name, "тікає зі швидкістю", self.speed)
+
+    def hunt(self, prey):
+        print(self.name, "не їсть м'ясо, травоїдний")
+
+
+class Lion(Predator):
+    def __init__(self, name, age, power, claw_damage, mane_size):
+        super().__init__(name, age, power, claw_damage)
+        self.mane_size = mane_size
+
+    def sound(self):
+        print(self.name, "гучно реве: Рррр!")
+
+    def info(self):
+        super().info()
+        print("Розмір гриви:", self.mane_size)
+
+
+class Tiger(Predator):
+    def __init__(self, name, age, power, claw_damage, stripes):
+        super().__init__(name, age, power, claw_damage)
+        self.stripes = stripes
+
+    def sound(self):
+        print(self.name, "гарчить: Рррр!")
+
+    def info(self):
+        super().info()
+        print("Кількість смуг:", self.stripes)
+
+
+class Wolf(Predator):
+    def __init__(self, name, age, power, claw_damage, pack_size):
+        super().__init__(name, age, power, claw_damage)
+        self.pack_size = pack_size
+
+    def sound(self):
+        print(self.name, "виє: Аууу")
+
+    def info(self):
+        super().info()
+        print("Розмір зграї:", self.pack_size)
+
+    def pack_hunt(self, prey):
+        print("Зграя з", self.pack_size, "вовків полює на", prey.name)
+        total_damage = (self.power + self.claw_damage) * self.pack_size
+        prey.take_damage(total_damage)
+
+
+class Wildebeest(Herbivorous):
+    def __init__(self, name, age, power, speed, herd_size):
+        super().__init__(name, age, power, speed)
+        self.herd_size = herd_size
+
+    def sound(self):
+        print(self.name, "мукає: Мууу")
+
+    def info(self):
+        super().info()
+        print("Розмір стада:", self.herd_size)
+
+
+class Hare(Herbivorous):
+    def __init__(self, name, age, power, speed, ear_length):
+        super().__init__(name, age, power, speed)
+        self.ear_length = ear_length
+
+    def sound(self):
+        print(self.name, "пищить: Пі-пі-пі!")
+
+    def info(self):
+        super().info()
+        print("Довжина вух:", self.ear_length)
+
+    def hide(self):
+        print(self.name, "ховається в кущах")
+
+
+class Deer(Herbivorous):
+    def __init__(self, name, age, power, speed, antler_size):
+        super().__init__(name, age, power, speed)
+        self.antler_size = antler_size
+
+    def sound(self):
+        print(self.name, "трубить: І-го-го!")
+
+    def info(self):
+        super().info()
+        print("Розмір рогів:", self.antler_size)
+
+    def kick(self):
+        print(self.name, "б'є копитом")
+
+
+print("Система полювання")
+
+lion = Lion("Лев", 5, 40, 25, "велика")
+tiger = Tiger("Тигр", 4, 45, 30, "95")
+wolf = Wolf("Вовк", 3, 30, 20, 5)
+
+wildebeest = Wildebeest("Антілопа", 2, 10, 80, 20)
+hare = Hare("Заєць", 1, 5, 90, 15)
+deer = Deer("Олень", 3, 12, 70, 30)
+
+print("\nІнформація про хижаків")
+for predator in [lion, tiger, wolf]:
+    predator.info()
+    predator.sound()
+
+print("\nІнформація про травоїдних")
+for herb in [wildebeest, hare, deer]:
+    herb.info()
+    herb.sound()
+
+print("\nСистема полювання")
+lion.hunt(wildebeest)
+tiger.hunt(deer)
+wolf.pack_hunt(hare)
+hare.hide()
+deer.kick()
+
+
+class BankAccount:
+    def __init__(self, money):
+        self.money = money
+
+    def withdraw(self, amount):
+        if amount == 0:
+            raise ValueError("Сума зняття не може дорівнювати 0")
+
+        if amount < 0:
+            raise ValueError("Не можна знімати мінус")
+
+        if amount > self.money:
+            credit = amount - self.money
+            print(f"Не вистачає {credit} грн")
+            days = int(input("На скільки днів взяти кредит? "))
+
+            if days <= 30:
+                percent = credit * 0.01 * days
+                rate = "1%"
+            else:
+                percent = credit * 0.04
+                rate = "4%"
+
+            answer = input(f"Взяти кредит {credit} грн під {rate} (відсотки: {percent} грн)? (так/ні): ")
+
+            if answer.lower() == "так":
+                self.money += credit
+                self.money -= percent
+                self.money -= amount
+                print(f"Знято {amount}. Залишок: {self.money}")
+            else:
+                print("Кредит відхилено")
+            return
+
+        self.money -= amount
+        print("Знято:", amount)
+        print("Залишок:", self.money)
+
+    def add_money(self, amount):
+        if amount < 0:
+            raise ValueError("Не можна додати від'ємну суму")
+        self.money += amount
+        print("Додано:", amount)
+        print("Залишок:", self.money)
+
+
+account = BankAccount(100)
+
+try:
+    take = int(input("\nСкільки зняти? "))
+    account.withdraw(take)
+
+except ValueError as e:
+    print("Помилка:", e)
